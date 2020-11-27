4d23f3f523d0db55e4e95e989689cbfac02d15af
Will Lane <williamlane923@gmail.com>
2020-11-26 19:26:34 -0800

    added file processing and made it slightly work

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"os"
+)
+
+var grayscale = []string{"$", "@", "B", "%", "8", "&", "W", "M", "#", "*", "o", "a", "h", "k", "b", "d", "p", "q", "w", "m", "Z", "O", "0", "Q", "L", "C", "J", "U", "Y", "X", "z", "c", "v", "u", "n", "x", "r", "j", "f", "t", "/", "\"", "\\", "|", "(", ")", "1", "{", "}", "[", "]", "?", "-", "_", "+", "~", "<", ">", "i", "!", "l", "I", ";", ":", ",", "\"", "^", "`", "'", "."}
+var (
+	width  = 0
+	height = 0
+)
+
+func main() {
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+
+	file, err := os.Open("./image.png")
+
+	if err != nil {
+		fmt.Println("Error: File could not be opened")
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	pixels, err := getPixels(file)
+
+	if err != nil {
+		fmt.Println("Error: Image could not be decoded")
+		os.Exit(1)
+	}
+
+	for i, v := range pixels {
+		for index, a := range v {
+			if index%2 == 0 && i%2 == 0 {
+				fmt.Print(grayscale[a.R/4], " ")
+			}
+		}
+		if i%2 == 0 {
+			fmt.Print("\n")
+		}
+	}
+}
+
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width, height = bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+
+	return pixels, nil
+}
+
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
+}
